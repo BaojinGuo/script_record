c4245c801e91eb88693f9ec227d9462e6e533b98
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-02-19 16:34:19 +0800

    Update Assembly_comparation.R

--- Assembly_comparation.R
+++ Assembly_comparation.R
@@ -1,5 +1,5 @@
-# Align assembly genomes
-minimap2 -t 128 -x asm5 /scratch/pawsey0399/bguo1/0.assembly/01.hifi_assembly/S2_HIFI_RESULT/S2_hifi.asm.bp.p_ctg.fa /scratch/pawsey0399/bguo1/0.assembly/01.hifi_assembly/S1_HIFI_RESULT/S1_hifi.asm.bp.p_ctg.fa >S1_TO_S2.paf
+### Previous step: Align assembly genomes
+## minimap2 -t 128 -x asm5 /scratch/pawsey0399/bguo1/0.assembly/01.hifi_assembly/S2_HIFI_RESULT/S2_hifi.asm.bp.p_ctg.fa /scratch/pawsey0399/bguo1/0.assembly/01.hifi_assembly/S1_HIFI_RESULT/S1_hifi.asm.bp.p_ctg.fa >S1_TO_S2.paf
 
 # Load necessary libraries
 library(pafr)      # For handling PAF (Pairwise mApping Format) files
