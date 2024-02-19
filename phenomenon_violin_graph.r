49f0b16eb8136cfdf8b778f69c28ce0b97075025
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-02-19 15:02:07 +0800

    Update phenomenon_violin_graph.r

--- phenomenon_violin_graph.r
+++ phenomenon_violin_graph.r
@@ -3,20 +3,30 @@ library(ggplot2)
 data<-read.csv("anova.csv",header = T)
 
 head(data)
-  Environment        Genotype TKW
-1         BDT 17MUB002-SSD002 199
-2         BDT 17MUB002-SSD003 164
-3         BDT 17MUB002-SSD004 157
-4         BDT 17MUB002-SSD005 191
-5         BDT 17MUB002-SSD006 193
-6         BDT 17MUB002-SSD007 219
+#  Environment  Genotype  TKW
+#  BDT  17MUB002-SSD002  199
+#  BDT  17MUB002-SSD003  164
+#  BDT  17MUB002-SSD004  157
+#  BDT  17MUB002-SSD005  191
+#  BDT  17MUB002-SSD006  193
+#  BDT  17MUB002-SSD007  219
 
-data$Environment<-as.factor(data$Environment)
-data$TKW<-as.numeric(data$TKW)
+# Convert the "Environment" column to a factor variable
+data$Environment <- as.factor(data$Environment)
 
-ggplot(data , aes(x = Environment, y = TKW, fill = Environment)) +
-    geom_violin(trim = FALSE) +
-    geom_boxplot(width = 0.1, fill = "white", color = "black") +
-    labs(x = "Environment", y = "Thousand Kernel Weight (g)") +
-    theme_minimal()
-    +theme(text = element_text(family = "Times New Roman", size = 16, face = "bold"))
+# Convert the "TKW" column to numeric
+data$TKW <- as.numeric(data$TKW)
+
+# Use ggplot2 to create a violin plot and boxplot
+ggplot(data, aes(x = Environment, y = TKW, fill = Environment)) +
+  geom_violin(trim = FALSE) +  # Add a violin plot without trimming the tails
+  geom_boxplot(width = 0.1, fill = "white", color = "black") +  # Add a boxplot with specified width and styling
+  
+  # Add labels to the axes
+  labs(x = "Environment", y = "Thousand Kernel Weight (g)") +
+  
+  # Apply a minimal theme to the plot
+  theme_minimal() +
+  
+  # Modify font style using Times New Roman, size 16, and bold
+  theme(text = element_text(family = "Times New Roman", size = 16, face = "bold"))
