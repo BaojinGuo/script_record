877d8f113e628245191648effd7b76b4c60a373f
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-05-02 09:42:07 +0800

    Update 01.minimap2.sh

--- Barley_Structure_variations/02.Sniffles/01.minimap2.sh
+++ Barley_Structure_variations/02.Sniffles/01.minimap2.sh
@@ -1,1 +1,15 @@
-
+ls 00.Hifi-data/*|cut -f2 -d "/"|while read line
+do filename=$(basename "$line")
+prefix=${filename%.fastq.gz}
+echo '#!/bin/bash
+#SBATCH --job-name=Mini
+#SBATCH --partition=work
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=24:00:00
+#SBATCH --account=pawsey0399
+module load singularity/3.11.4-nompi
+module load samtools/1.15--h3843a85_0
+srun --export=all -n 1 -c 128 singularity exec /scratch/pawsey0399/bguo1/Singularity_image/minimap2.sif minimap2 -ax map-pb -I100g -t 128 Morex.V3.chr.fasta 00.Hifi-data/'"$line"' |samtools sort -@ 128 -O BAM -o 01.minimap/'"$prefix"'.Morex.sort.bam' >$prefix.mini.sh
+done
