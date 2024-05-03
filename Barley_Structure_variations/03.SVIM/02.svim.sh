e5fdf5c511e906bbed9b0def53b071888ac97405
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-05-03 11:12:12 +0800

    Update 02.svim.sh

--- Barley_Structure_variations/03.SVIM/02.svim.sh
+++ Barley_Structure_variations/03.SVIM/02.svim.sh
@@ -11,5 +11,6 @@ filename=$(basename "$line"); prefix=${filename%.Morex.sort.bam}; echo '#!/bin/b
 #SBATCH --time=24:00:00
 #SBATCH --account=pawsey0399
 source /scratch/pawsey0399/bguo1/software/miniconda/bin/activate svim
-srun --export=all -n 1 -c 128 svim alignment 02.results/'"$prefix"' 01.minimap/'"$line"' Morex.V3.chr.fasta --symbolic_alleles  --min_sv_size 50 --sample '"$prefix"' '>$prefix.svim.sh
+srun --export=all -n 1 -c 128 svim alignment 02.results/'"$prefix"' 01.minimap/'"$line"' Morex.V3.chr.fasta  --min_sv_size 50 --sample '"$prefix"' '>$prefix.svim.sh
 done
+##don't add the parameter --symbolic_alleles due to SURVIVOR does not support implicit ALT
