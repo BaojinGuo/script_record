d5eeb79465c1b0f8e8e0b63223a4bceb6f546190
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-05-04 11:09:23 +0800

    Update 02.cuteSV.sh

--- Barley_Structure_variations/05.cuteSV/02.cuteSV.sh
+++ Barley_Structure_variations/05.cuteSV/02.cuteSV.sh
@@ -9,3 +9,6 @@ ls 01.minimap/*.bam|cut -f2 -d "/"|while read line; do filename=$(basename "$lin
 source /scratch/pawsey0399/bguo1/software/miniconda/bin/activate cutesv
 srun --export=all -n 1 -c 128 cuteSV 01.minimap/'"$line"' Morex.V3.chr.fasta 02.result/'"$prefix"'.cuteSV.vcf work_dictionary/'"$prefix"' -s 1 --max_cluster_bias_INS 1000 --diff_ratio_merging_INS 0.9 --max_cluster_bias_DEL 1000 --diff_ratio_merging_DEL 0.5 -t 128' >$prefix.cuteSV.sh
 done
+
+###merge all vcfs
+SURVIVOR merge 02.result/ 1000 1 1 1 0 50 merge_cuteSV.vcf
