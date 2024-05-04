c89507988b94018eb7b06183d4407ae606b702a7
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-05-04 10:19:09 +0800

    Update 02.sniffles.sh

--- Barley_Structure_variations/02.Sniffles/02.sniffles.sh
+++ Barley_Structure_variations/02.Sniffles/02.sniffles.sh
@@ -12,3 +12,36 @@ ls 01.minimap/*.bam|cut -f2 -d "/"|while read line; do filename=$(basename "$lin
 source /scratch/pawsey0399/bguo1/software/miniconda/bin/activate sniffle
 srun --export=all -n 1 -c 128 sniffles --input 01.minimap/'"$line"' --vcf 02.sniffles/'"$prefix"'.Morex.vcf --reference Morex.V3.chr.fasta --snf 02.sniffles/'"$prefix"'.Morex.snf --threads 128 --minsupport 1 --long-ins-length 100000000   --long-del-length 100000000' >$prefix.sniffle.sh 
 done
+
+
+###multisamples
+srun -c 128 -n 1 sniffles --input list.tsv --vcf 02.sniffles/combineSV_sniffle.vcf --reference Morex.V3.chr.fasta --threads 128 --minsupport 1 --long-ins-length 100000000 --long-del-length 100000000
+###list.tsv----all snf files of samples
+02.sniffles/Baudin.Morex.snf
+02.sniffles/Beecher.Morex.snf
+02.sniffles/Betzes.Morex.snf
+02.sniffles/Buff.Morex.snf
+02.sniffles/Buloke.Morex.snf
+02.sniffles/Clipper.Morex.snf
+02.sniffles/Compass.Morex.snf
+02.sniffles/Eth69.Morex.snf
+02.sniffles/Fathom.Morex.snf
+02.sniffles/Flagship.Morex.snf
+02.sniffles/Gairdner.Morex.snf
+02.sniffles/Galleon.Morex.snf
+02.sniffles/Halcyon.Morex.snf
+02.sniffles/Harrington.Morex.snf
+02.sniffles/ILAN15.Morex.snf
+02.sniffles/Laperouse.Morex.snf
+02.sniffles/LaTrobe.Morex.snf
+02.sniffles/Leabrook.Morex.snf
+02.sniffles/Maximus.Morex.snf
+02.sniffles/Mundah.Morex.snf
+02.sniffles/Prior.Morex.snf
+02.sniffles/RGT_Planet.Morex.snf
+02.sniffles/Sahara3771.Morex.snf
+02.sniffles/Schooner.Morex.snf
+02.sniffles/Stirling.Morex.snf
+02.sniffles/Vlamingh.Morex.snf
+02.sniffles/Yambla.Morex.snf
+02.sniffles/Yeti.Morex.snf
