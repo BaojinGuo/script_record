5940e50944b8b953d7ab3b7752d41ac3efd0f5d3
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-05-10 09:37:31 +0800

    Update 05.unique_insertion.sh

--- Wheat_Structure_Variation/01.SyRI/05.unique_insertion.sh
+++ Wheat_Structure_Variation/01.SyRI/05.unique_insertion.sh
@@ -12,3 +12,4 @@ awk '$8 - $7 > 10000 && $8 - $7 < 50000 {print $0}' 3504_CS21syri.unique_inserti
 cut -f6,7,8 3504_unique_insertion.10k_to_50k.out >3504_unique_insertion.10k_to_50k.bed
 bedtools getfasta -fi ../../../3504_map_field/3504.v1.fa -bed 3504_unique_insertion.10k_to_50k.bed -fo 3504_unique_insertion.10k_to_50k.fa
 
+srun --export=all -n 1 -c 128 singularity exec /scratch/pawsey0399/bguo1/minimap2.sif minimap2 -x asm5 -I100g --cs=long --secondary=no -t 128 Ac.1gChr.fa 3504_unique_insertion.200_to_1k.fa >3504_1chr.200-1k.asm5.paf
