1de1a0fa303ef5419107df773d6380b72a7b070b
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-06-08 18:10:57 +0800

    Update Rawscript.sh

--- Compare_HIFI_Illumina/Rawscript.sh
+++ Compare_HIFI_Illumina/Rawscript.sh
@@ -136,8 +136,32 @@ source /scratch/pawsey0399/bguo1/software/miniconda/bin/activate svim
 srun --export=all -n 1 -c 128 svim alignment 03.SVIM/'"$prefix"' 01.minimap/'"$line"' MorexV3.fa  --minimum_depth 5 --min_sv_size 50 --sample '"$prefix"' '>$prefix.svim.sh; done
 
 
+###sr minimap
+ls 1.split/*|cut -f2 -d "/"|cut -f1 -d "_"|sort|uniq|while read line; do echo '#!/bin/bash
+#SBATCH --job-name=Mini
+#SBATCH --partition=work
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=24:00:00
+#SBATCH --account=pawsey0399
+module load singularity/4.1.0-nompi
+module load samtools/1.15--h3843a85_0
+srun --export=all -n 1 -c 128 singularity exec /scratch/pawsey0399/bguo1/Singularity_image/minimap2.sif minimap2 -ax sr -c --MD -R '"'@RG\tID:"$line"\tSM:"$line"'"' -I100g -t 128 -Y MorexV3.fa 1.split/'"$line"'_clean_1.fq.gz 1.split/'"$line"'_clean_2.fq.gz |samtools sort -@ 128 -O BAM -o 01.minimap/'"$line"'.Morex.sr.sort.bam' >$line.srmini.sh; done
 
 
+##lr minimap
 
+ls 1.split/*.fastq.gz|cut -f2 -d"/"|cut -f1 -d "."|while read line; do echo '#!/bin/bash
+#SBATCH --job-name=Mini
+#SBATCH --partition=work
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=24:00:00
+#SBATCH --account=pawsey0399
+module load singularity/4.1.0-nompi
+module load samtools/1.15--h3843a85_0
+srun --export=all -n 1 -c 128 singularity exec /scratch/pawsey0399/bguo1/Singularity_image/minimap2.sif minimap2 -ax map-hifi -c --MD -R '"'@RG\tID:"$line"\tSM:"$line"'"' -I100g -t 128 -Y MorexV3.fa 1.split/'"$line"'.fastq.gz |samtools sort -@ 128 -O BAM -o 01.minimap/'"$line"'.Morex.sort.bam' >$line.mini.sh; done
 
 
