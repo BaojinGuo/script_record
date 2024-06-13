09d65dc7474674513e1a0ca6c203ec9154a61167
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-06-13 13:22:50 +0800

    Update barplot.R

--- Compare_HIFI_Illumina/01.Benchmark/barplot.R
+++ Compare_HIFI_Illumina/01.Benchmark/barplot.R
@@ -20,7 +20,7 @@ combined_data <- do.call(rbind, data_list)
 
 # 设置文件列的因子变量，确保顺序和文件名一致
 combined_data$file <- factor(combined_data$file, levels = file_names)
-
+combined_data$file<-gsub("\\.summary", "", combined_data$file)
 # 创建左右两个Y轴的范围
 y1_max <- max(combined_data$Freq[combined_data$Type %in% c("DEL", "INS")])
 y2_max <- max(combined_data$Freq[combined_data$Type %in% c("INV", "DUP")])
@@ -28,14 +28,14 @@ y2_max <- max(combined_data$Freq[combined_data$Type %in% c("INV", "DUP")])
 # 绘制第一个图表（DEL和INS）
 p1 <- ggplot(combined_data, aes(x = Type, y = Freq, fill = file)) +
     geom_bar(data = combined_data[combined_data$Type %in% c("DEL", "INS"), ], stat = "identity", position = "dodge", width = 0.7) +
-    scale_fill_manual(values = colors, breaks = file_names) +
+    scale_fill_manual(values = colors) +
     theme_minimal(base_size = 16) +
     theme(axis.text.x = element_text(size = 16, family = "Times New Roman"),
           axis.text.y = element_text(size = 16, family = "Times New Roman")) +
     scale_y_continuous(name = "", limits = c(0, y1_max)) +  # 移除Y轴标题
     
     # 设置图例
-    guides(fill = guide_legend(title = "File")) +  # 设置图例标题
+    guides(fill = "none") +  # 设置图例标题
     
     # 移除X轴标题
     labs(x = "")  # 移除X轴标题
@@ -43,16 +43,15 @@ p1 <- ggplot(combined_data, aes(x = Type, y = Freq, fill = file)) +
 # 绘制第二个图表（INV和DUP）
 p2 <- ggplot(combined_data, aes(x = Type, y = Freq, fill = file)) +
     geom_bar(data = combined_data[combined_data$Type %in% c("INV", "DUP"), ], stat = "identity", position = "dodge", width = 0.7) +
-    scale_fill_manual(values = colors, breaks = file_names) +
+    scale_fill_manual(values = colors) +
     theme_minimal(base_size = 16) +
     theme(axis.text.x = element_text(size = 16, family = "Times New Roman"),
           axis.text.y = element_text(size = 16, family = "Times New Roman")) +
-    scale_y_continuous(name = "", limits = c(0, y2_max), sec.axis = sec_axis(~ ., name = NULL)) +  # 移除Y轴标题
-    
-    # 移除X轴标题
+    scale_y_continuous(name = "", limits = c(0, y2_max)) +  # 移除Y轴标题
+    guides(fill = guide_legend(title = "")) +
     labs(x = "")  # 移除X轴标题
 
-# 将两个图表左右拼接，并共享一个图例
+# 将两个图表左右拼接
 combined_plot <- p1 + p2 + plot_layout(guides = 'collect')
 
 # 去除标题和其他可能会导致重叠的元素
