2799c41c9d55fee61617488e6cdd70d4a6e0a8c9
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-07-17 16:51:19 +0800

    Update 90barley-wgs.sh

--- 1.NGS_data_script/90barley-wgs.sh
+++ 1.NGS_data_script/90barley-wgs.sh
@@ -11,3 +11,7 @@ vcftools --gzvcf barley_1H.cohort.vcf.gz --remove-indels --maf 0.25 --max-missin
 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n' barley_1H.snp.filter.record.vcf |awk -v OFS="\t" '{het=0;hom=0;for(i=5;i<=NF;i++) if($i=="0/1" || $i=="1/0") het+=1; else hom+=1;} {if(het/(het+hom)<0.1) print $1,$2;}' > low_het_sites.txt
 
 bcftools view -T low_het_sites.txt barley_1H.snp.filter.recode.vcf -Oz -o 1H_filtered.vcf.gz
+
+bcftools index 1H_filtered.vcf.gz
+
+bcftools merge -Oz -o merged.vcf.gz 1H_filtered.vcf.gz 2H_filtered.vcf.gz
