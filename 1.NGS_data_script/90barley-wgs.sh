29c1b5b86266acd395e8fdbe4922f179e31bd905
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-07-22 09:28:14 +0800

    Update 90barley-wgs.sh

--- 1.NGS_data_script/90barley-wgs.sh
+++ 1.NGS_data_script/90barley-wgs.sh
@@ -13,5 +13,8 @@ bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n' barley_1H.snp.filter.recor
 bcftools view -T low_het_sites.txt barley_1H.snp.filter.recode.vcf -Oz -o 1H_filtered.vcf.gz
 
 bcftools index 1H_filtered.vcf.gz
+###merge same samples different chromosome
+gatk MergeVcfs -I 1H/1H_filtered.vcf.gz -I 2H/2H_filtered.vcf.gz -I 3H/3H_filtered.vcf.gz -I 4H/4H_filtered.vcf.gz -I 5H/5H_filtered.vcf.gz -I 6H/6H_filtered.vcf.gz -I 7H/7H_filtered.vcf.gz -O barley90_allchr.snp.vcf
 
+###combine same chromosome different samples
 bcftools merge -Oz -o merged.vcf.gz 1H_filtered.vcf.gz 2H_filtered.vcf.gz
