7d708eba7f431f9c0545ee52e33a42b3a5a0b26a
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2024-11-18 09:10:21 +0800

    Update Rna-seq.pipeline.sh

--- 08.He/Rna-seq.pipeline.sh
+++ 08.He/Rna-seq.pipeline.sh
@@ -9,11 +9,11 @@ ls *_1.clean.fq.gz|while read line; do base_name=$(basename "$line" "_1.clean.fq
 #SBATCH --partition=work
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=64
 #SBATCH --time=12:00:00
 #SBATCH --account=pawsey0399
 source /scratch/pawsey0399/bguo1/software/miniconda/bin/activate hisat
-srun --export=all -n 1 -c 32 hisat2 -x 0.Index/CS2Z559P1.fa -1 '${base_name}'_1.clean.fq.gz -2 '${base_name}'_2.clean.fq.gz -S '${base_name}'.sam'  > ${base_name}.hisat.sh;  done
+srun --export=all -n 1 -c 64 hisat2 --dta -x 0.Index/CS2Z559P1.fa -1 '${base_name}'_1.clean.fq.gz -2 '${base_name}'_2.clean.fq.gz -S '${base_name}'.sam'  > ${base_name}.hisat.sh;  done
 
 
 
