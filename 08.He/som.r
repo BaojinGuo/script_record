0f83017d56f7b732929c8b76c650b4aec75d12de
BaojinGuo <156871472+BaojinGuo@users.noreply.github.com>
2025-02-05 14:40:19 +0800

    Update som.r

--- 08.He/som.r
+++ 08.He/som.r
@@ -28,7 +28,7 @@ custom_palette <- function(n) {
   return(colors)
 }
 # 可视化SOM结果
-plot(som_model, type = "codes")  # 查看神经元权重向量
+plot(som_model, type = "codes",palette.name=custom_palette)  # 查看神经元权重向量
 plot(som_model, type = "counts") # 查看每个单元中的样本数量
 plot(som_model, type = "mapping") # 显示样本的映射情况
 plot(som_model, type = "dist.neighbours") # 显示神经元间距离
@@ -42,3 +42,46 @@ plot(som_model, type = "property", property = som_cluster, main = "SOM Clusterin
 
 # 保存结果
 write.csv(som_model$codes[[1]], "som_cluster_centroids.csv")
+
+
+
+#####
+# `som_model$unit.classif` 存储了每个样本被分配到哪个神经元（索引）
+sample_to_neuron <- data.frame(
+  Sample = rownames(data),  # 样本名称
+  Neuron = som_model$unit.classif  # 对应的神经元编号
+)
+
+# 查看结果
+print(sample_to_neuron)
+
+# 保存为 CSV 文件
+write.csv(sample_to_neuron, "sample_to_neuron.csv", row.names = FALSE)
+
+
+
+####plot individual block
+neuron_17 <- som_model$codes[[1]][17, ]
+neuron_17_df <- data.frame(
+    Sample = colnames(som_model$codes[[1]]),
+    Value = neuron_17
+)
+
+neuron_17_df$Normalized_Value <- (neuron_17_df$Value - min(neuron_17_df$Value)) / 
+    (max(neuron_17_df$Value) - min(neuron_17_df$Value))
+
+
+# 确保 Sample 按原顺序绘制（转换为 factor，并保持顺序）
+neuron_17_df$Sample <- factor(neuron_17_df$Sample, levels = neuron_17_df$Sample)
+
+ggplot(neuron_17_df, aes(x = Sample, y = Normalized_Value, fill = Sample)) +
+    geom_bar(stat = "identity", width = 1, color = "white") +
+    coord_polar(theta = "x") +  # theta="y" 适用于扇形图
+    theme_void() +
+    scale_fill_manual(values = custom_palette2)  +
+geom_hline(yintercept = seq(0, 1, by = 0.2), color = "black", linetype = "dashed", alpha = 0.5)
+
+
+
+
+
